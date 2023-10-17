145287a3bab11616486f75bae6ef9ba49487af99
aryantyagi00 <131746093+aryantyagi00@users.noreply.github.com>
2023-10-17 22:55:34 +0530

    Add files via upload

---
+++ pattern/rhombus.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class rhombus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int  i=1;i<=5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+        for(int k=1;k<=5;k++){
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+}
